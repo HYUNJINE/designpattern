79114013af1c76ea3827256b7def318e74b24cbe
hyunjin <leehj0110@kakao.com>
2020-11-17 10:39:34 +0900

    20-11-17

---
+++ php/Builder/Memory.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class Memory 
+{
+    private $size;
+
+    public function __construct($size = null)
+    {
+        if($size) {
+            $this->size = $size;
+        }
+
+    }
+
+    public function setSize($size) 
+    {
+        $this->size =$size;
+    }
+
+    public function getSize() 
+    {
+        return $this->size;
+    }
+}
